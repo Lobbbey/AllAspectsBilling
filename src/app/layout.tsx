98f7623a4ca51575270385df581671f09553c26a
Lobbbey <joshuatylerbandy@gmail.com>
2025-08-28 21:26:52 -0400

    changed around navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-prussian-blue`}>
         < NavBar />
         {children}
         <Analytics />
