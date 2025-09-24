15e089b9ccf38924083510b38b4f40adc63e2dd7
Lobbbey <joshuatylerbandy@gmail.com>
2025-09-24 18:34:27 -0400

    added privacy policy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,9 +1,17 @@
+import Script from 'next/script';
 
-export default function privacyPolicy(){
+export default function PrivacyPolicy() {
   return(
-  <section className="flex flex-col gap-12 py-24">
-      <h2 className="text-2xl text-center"> Coming Soon </h2>
-  </section>
-  );
+    <section className="bg-gray-100 py-24">
 
+      <Script
+        id="usercentrics-ppg"
+        src="https://policygenerator.usercentrics.eu/api/privacy-policy"
+        privacy-policy-id="ab663eeb-03ad-4c41-88b0-30631d4b19fb"
+        strategy="afterInteractive"
+      />
+      
+      <div className="uc-privacy-policy text-left"></div>
+    </section>
+  );
 }
