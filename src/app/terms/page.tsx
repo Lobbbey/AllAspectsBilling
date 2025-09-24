5935ea1fcc95d4d91e6b6fd2204cd12aafdea2df
Lobbbey <joshuatylerbandy@gmail.com>
2025-09-24 12:29:53 -0400

    Created Temp pages

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,9 @@
+
+export default function terms(){
+  return(
+  <section className="flex flex-col gap-12 py-24">
+      <h2 className="text-2xl text-center"> Coming Soon </h2>
+  </section>
+  );
+
+}
