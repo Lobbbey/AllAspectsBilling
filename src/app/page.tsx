42540765bd8822e21cf0c7ac9462dde32aa89f3b
Lobbbey <joshuatylerbandy@gmail.com>
2025-10-07 13:43:25 -0400

    Added Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ import About from "@/components/about";
 export default function Home() {
   return (
     <section className="flex flex-col gap-12">
+      <div className="w-full h-full relative">
+        <img
+          src="/banner.png"
+          alt="A person working on a computer with multiple monitors"
+          className="w-full max-w-7xl h-full mx-auto"
+        />
+      </div>
       <Services />
       <About />
     </section>
--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,9 +1,8 @@
 
 export default function terms(){
-  return(
-  <section className="flex flex-col gap-12 py-24">
-      <h2 className="text-2xl text-center"> Coming Soon </h2>
-  </section>
-  );
-
+    return(
+        <section>
+            
+        </section>
+    );
 }
